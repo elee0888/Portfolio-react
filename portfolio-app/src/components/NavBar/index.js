9cace539d1fc93e0db0238ad3a03877aef0ab85c
elee0888 <e.lee@my.ccsu.edu>
2020-04-17 11:00:25 -0400

    added a card component and a projects.js file

--- portfolio-app/src/components/NavBar/index.js
+++ portfolio-app/src/components/NavBar/index.js
@@ -5,17 +5,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
-      <button
-        className="navbar-toggler navbar-light"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
@@ -24,26 +14,13 @@ function Navbar() {
               <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li className="nav-item dropdown">
-            <Link
-              to="/"
-              className="nav-link dropdown-toggle"
-              data-toggle="dropdown"
-              role="button"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Portfolio <i className="fa fa-archive"></i>
-            </Link>
-            <div className="dropdown-menu">
-              <a className="dropdown-item" href="/Design">
-                <i className="fas fa-terminal"></i> Web Development Projects
-              </a>
-              <a className="dropdown-item" href="/Resume">
-                <i className="far fa-file"></i> Resume
-              </a>
-            </div>
+
+          <li className="nav-item">
+            <a className="nav-link" href="/Portfolio">
+              <i className="fas fa-address-book"></i> Portfolio
+            </a>
           </li>
+          
           <li className="nav-item">
             <a className="nav-link" href="/Contact">
               <i className="fas fa-address-book"></i> Contact
---
+++ portfolio-app/src/components/Projects.js
@@ -0,0 +1,113 @@
+export default [
+    {
+      id: 1,
+      image: require("./images/Lively.png"),
+      card_title: "Lively",
+      card_text:
+        "A music app that allows users to find theyir favorite song or artist and see what their top played song is. Along with other related artist is displayed as well.",
+      technologies: [
+        "JavaScript",
+        "JQUERY",
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "Live.fm API",
+        "TasteDiv API"
+      ],
+      project_repo: "https://github.com/Sforzemon/Lively",
+      deployed_project: "http://sforzemon.github.io/Lively/"
+    },
+    {
+      id: 2,
+      image: require("./images/ShopWise.PNG"),
+      card_title: "ShopWise",
+      card_text:
+        "A ecommerce website that will let users shop for products. Displaying the inforation about the product for the user. Also can allow the user to add the product to the cart as well. Further updates may soon to come.",
+      technologies: [
+        "Ebay API",
+        "Amazon API",
+        "MySql",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "jQuery",
+        "Heroku"
+      ],
+      project_repo: "https://github.com/EMaynard802/Project2",
+      deployed_project: "Coming Soon!"
+    },
+    {
+      id: 3,
+      image: require("./images/developer-profile-generator.PNG"),
+      card_title: "Devloper Profile Generator",
+      card_text:
+        "This app uses Node.js to generate a HTML file by pulling a user's profile information from GitHub, then creates a PDF file from that HTML file.",
+      technologies: [
+        "Node",
+        "NPM",
+        "JavaScript",
+        "HTML",
+        "CSS"
+      ],
+      project_repo: "https://github.com/elee0888/Developer-Profile-Generator",
+      deployed_project: "https://elee0888.github.io/Developer-Profile-Generator/"
+    
+    },
+    {
+      id: 4,
+      image: require("./images/Capture.PNG"),
+      card_title: "Day Planner",
+      card_text:
+        "Plan for your day using the time blocks corresponding to typical business workday (9AM - 9PM). After inputting a plan or description into the appropriate block be sure to save using the side blue button with the lock icon. This will save the inputted tasks for that block into local storage and will re-populate even when page is refreshed.",
+      technologies: [
+        "Moment.js library",
+        "local storage",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Fontawesome",
+        "Bootstrap"
+      ],
+      project_repo: "https://github.com/elee0888/Day-Planner2",
+      deployed_project: "https://elee0888.github.io/Day-Planner2/"
+    },
+    {
+      id: 5,
+      image: require("./images/hot-restaurant.PNG"),
+      card_title: "Hot Restaurant",
+      card_text:
+        "A app to help schedule reservation requests. Restaurant has just 5 tables available. First five requests get a reservation, every request after that is sent to the waiting list.",
+      technologies: [
+        "Node",
+        "Express",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "jQuery",
+        "AJAX",
+        "Heroku"
+      ],
+      project_repo: "https://github.com/elee0888/HotRestaurant",
+      deployed_project: "https://hot-restaurant-fsf.herokuapp.com/"
+    },
+    {
+      id: 6,
+      image: require("./images/employee-directory.PNG"),
+      card_title: "Employee Tracker",
+      card_text:
+        "A React employee directory application allowing users to view all employees, sort employees by last name, phone number, or age, in ascending and descending order, as well as search for employees by name.",
+      technologies: [
+        "React",
+        "JSX",
+        "Node",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "RandomUser API",
+        "Heroku"
+      ],
+      project_repo: "https://github.com/elee0888/EmployeeDirectory",
+      deployed_project: "Coming Soon!"
+    }
+  ];
