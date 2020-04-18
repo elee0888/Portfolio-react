c0153187637237ef458b4665133965cf1336a3f1
elee0888 <e.lee@my.ccsu.edu>
2020-04-18 17:48:01 -0400

    updated navbar

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -10,6 +10,7 @@ import Wrapper from "./components/Wrapper";
 import Jumbotron from "./components/Jumbotron";
 
 
+
 function App (){
 
     return(
@@ -19,7 +20,7 @@ function App (){
         <Jumbotron />
         <Wrapper>
             <Route exact path= "/" component={About} />
-            <Route exact path= "/About" component={About} />
+            <Route path= "/About" component={About} />
             <Route path= "/Portfolio" component={Portfolio} />
             <Route path= "/Resume" component={Resume} />
             <Route path= "/Contact" component={Contact} />
--- portfolio-app/src/components/NavBar/index.js
+++ portfolio-app/src/components/NavBar/index.js
@@ -4,31 +4,56 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+     
+  <a className="navbar-brand" href="#">Eric Lee</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+       
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/About">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/Portfolio">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/Contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  //   <nav className="navbar navbar-expand-lg">
       
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="/About">
-              <i className="fas fa-user-tie"></i> About Me{" "}
-              <span className="sr-only">(current)</span>
-            </a>
-          </li>
+  //     <div className="collapse navbar-collapse" id="navbarNav">
+  //       <ul className="navbar-nav">
+  //         <li className="nav-item active">
+  //           <a className="nav-link" href="/About">
+  //             <i className="fas fa-user-tie"></i> About Me{" "}
+  //             <span className="sr-only">(current)</span>
+  //           </a>
+  //         </li>
 
-          <li className="nav-item">
-            <a className="nav-link" href="/Portfolio">
-              <i className="fas fa-address-book"></i> Portfolio
-            </a>
-          </li>
+  //         <li className="nav-item">
+  //           <a className="nav-link" href="/Portfolio">
+  //             <i className="fas fa-address-book"></i> Portfolio
+  //           </a>
+  //         </li>
           
-          <li className="nav-item">
-            <a className="nav-link" href="/Contact">
-              <i className="fas fa-address-book"></i> Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+  //         <li className="nav-item">
+  //           <a className="nav-link" href="/Contact">
+  //             <i className="fas fa-address-book"></i> Contact
+  //           </a>
+  //         </li>
+
+
+  //       </ul>
+  //     </div>
+  //   </nav>
+   );
 }
 export default Navbar;
